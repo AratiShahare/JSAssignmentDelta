ebaa92e5df1d7c8b59e5e7d0698fa7ffa1ce28d2
AratiShahare <shaharearati0@gmail.com>
2022-11-18 21:52:26 +0530

    file

---
+++ amansirAssig3.js
@@ -0,0 +1,75 @@
+// Q1. get a variable and increase/decrease it by one using unary operator
+ let x=10
+     x++
+     console.log(x)
+
+ let a=35
+     a--
+   console.log(a)    
+
+// Q2. proove the prefix and postfix unary operator by a program output.
+// a.prefix
+let c=65
+    b=c++
+    console.log(b)
+
+// b.postfix
+let d=32
+    e=++d
+    console.log(d)
+
+// Q3. get two variable and do some arithmatic operation with the values using arithmatic operator.
+num1=25
+num2=31
+
+addition=num1+num2
+console.log("addition",addition);
+
+substraction=num1-num2
+console.log("substraction",substraction);
+
+multiplication=num1*num2
+console.log("multiplication",multiplication);
+
+moduleOperator=num1%num2
+console.log("moduleOperator",moduleOperator);
+
+exponentiation=num1**num2
+console.log("exponentiation", exponentiation);
+
+// Q4. get a variable name career infotech and defined the 100 now assign this value to the next variable infotech.
+let career=100
+let infotech=career
+console.log(infotech)
+
+// Q5. get two variable name ci and info assign the numerical value to both and now compare the value using relational operator.
+let ci=40
+let info=79
+console.log(ci>info)
+console.log(ci<info)
+console.log(ci==info)
+console.log(ci===info)
+console.log(ci>=info)
+console.log(ci<=info)
+console.log(ci!=info)
+
+//  Q6. create a result (pass or failed) using ternory operator and use logical operator in that.
+// a. ternary operator
+ mark=40
+ result1=mark>35 ? "you are pass":"you are fail"
+ result2=mark<35 ? "you are pass":"you are fail"
+ console.log(result1)
+ console.log(result2)
+
+// b. logical operator
+//  1. && (and)
+ let n="arati"
+     m=12
+ let p=n=="arati" && m==12
+ console.log(p)
+
+ //  2. || (or)
+ let q="arati"
+     r=12
+ let s=q=="arati" || r==55
+ console.log(s)
